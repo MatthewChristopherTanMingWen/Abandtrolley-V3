7a9dc897b4103cdc589def1a5053ec98f66d7de0
Matthew Christopher Tan Ming Wen <mctmw2006@gmail.com>
2021-06-23 07:59:47 +0800

    Initial Commit

---
+++ Prototype V1/Prototype_V1App.swift
@@ -0,0 +1,17 @@
+//
+//  Prototype_V1App.swift
+//  Prototype V1
+//
+//  Created by Matthew Tan on 23/6/21.
+//
+
+import SwiftUI
+
+@main
+struct Prototype_V1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
