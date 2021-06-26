3e0a18c245209b12804a70c2c69a456f278d80db
Matthew Christopher Tan Ming Wen <mctmw2006@gmail.com>
2021-06-26 10:27:36 +0800

    Made main blank log in template screen and added sign up button

--- Prototype V1/ContentView.swift
+++ Prototype V1/ContentView.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var username: String = ""
+    @State var password: String = ""
+    @State var logInScreen = true
+    @State var signUpScreen = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            VStack{
+                HStack(alignment: .center) {
+                    Text("Username:")
+                        .font(.callout)
+                        .bold()
+                    
+                    TextField("Enter username...", text: $username)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+                HStack {
+                    Text("Password:")
+                        .font(.callout)
+                        .bold()
+                    
+                    TextField("Enter password...", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+            }.padding()
+            
+            HStack {
+                Button("Log in") {
+                    print(username)
+                }.padding()
+                
+                Button("No account? Sign up!"){
+                    if (signUpScreen == false){
+                        signUpScreen = true
+                    }
+                }.padding()
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
